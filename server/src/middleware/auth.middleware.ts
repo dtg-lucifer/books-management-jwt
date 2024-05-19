897b7f61ec113b1e855d064c8218375a41f4285f
Piush Bose <piushbasu@gmail.com>
2024-05-19 22:53:14 +0530

    code: started working on the frontend

--- server/src/middleware/auth.middleware.ts
+++ server/src/middleware/auth.middleware.ts
@@ -13,7 +13,7 @@ export const RequireAuth = (
   res: Response,
   next: NextFunction
 ) => {
-  const token = req.headers.authorization;
+  const token = req.headers.authorization?.split(" ")[1];
   if (!token) {
     return res.status(401).json({ message: "No token, authorization denied" });
   }
