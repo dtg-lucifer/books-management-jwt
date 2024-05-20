386e7922698db0d72b1e40cab20faf85cdde452f
Piush Bose <piushbasu@gmail.com>
2024-05-20 22:57:34 +0530

    code: scaffolding the project structure of the frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/home/home.page";
+import { BookShowcase } from "./pages/bookshowcase/books.page";
+import Login from "./pages/authentication/login.page";
+import Register from "./pages/authentication/register.page";
+
 function App() {
   return (
-    <div className="main">
-      Hello
-    </div>
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="/books" element={<BookShowcase />} />
+      <Route path="/auth">
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Route>
+      <Route path="*" element={<div>404 Not Found</div>} />
+    </Routes>
   );
 }
 
---
+++ client/src/pages/bookshowcase/books.page.tsx
@@ -0,0 +1,7 @@
+import styles from "./books.module.scss"
+
+const BookShowcase = () => {
+  return <div>BookShowcase</div>;
+};
+
+export { BookShowcase };
